b4bdec0aa67f2943e2662a9c1c755524d4f08d65
Alexander Tarlinder <alexander.tarlinder@crisp.se>
2016-12-15 22:12:24 +0100

    Assertions test for xunit.

---
+++ xunit/XUnit/AssertionsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTests
+{
+    public class AssertionsTest
+    {
+        [Fact]
+        public void BasicEquality()
+        {
+            const int expected = 42;
+            Assert.Equal(expected, 42);
+            Assert.NotEqual(expected, 43);
+        }
+
+        [Fact]
+        public void Ranges() {
+            Assert.InRange(42, 1, 42);
+            Assert.NotInRange(42, 1, 41);
+        }
+
+        [Fact]
+        public void Truth()
+        {
+            Assert.True(true);
+            Assert.False(false);
+        }
+
+        [Fact]
+        public void Nulls()
+        {
+            Assert.Null(null);
+            Assert.NotNull("null");
+        }
+    }
+}
---
+++ xunit/runconsole.bat
@@ -0,0 +1,2 @@
+@echo off
+packages\xunit.runner.console.2.1.0\tools\xunit.console XUnit\bin\Debug\XUnit.dll
